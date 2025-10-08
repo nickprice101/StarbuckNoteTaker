5e582a3d45c49bcb49aac846d0f1099202ffb027
Nick Price <47870097+nickprice101@users.noreply.github.com>
2025-10-08 07:50:45 +0200

    Fix missing nativeCanvas import

--- app/src/main/java/com/example/starbucknotetaker/ui/SketchPadDialog.kt
+++ app/src/main/java/com/example/starbucknotetaker/ui/SketchPadDialog.kt
@@ -41,6 +41,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.input.pointer.pointerInput
